7126d34f52dd0dba5ba1d634541d24cafa420268
admin <645256685@qq.com>
2016-12-15 20:28:36 +0800

    重用cell

---
+++ SGInfiniteView/LYInfiniteViewCell.h
@@ -0,0 +1,15 @@
+//
+//  LYInfiniteViewCell.h
+//  TaiYangHua
+//
+//  Created by admin on 16/12/15.
+//  Copyright © 2016年 hhly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYInfiniteViewCell : UIView
+/** reuseID */
+@property(nonatomic,copy) NSString *reuseID;
+
+@end
---
+++ SGInfiniteView/LYInfiniteViewCell.m
@@ -0,0 +1,13 @@
+//
+//  LYInfiniteViewCell.m
+//  TaiYangHua
+//
+//  Created by admin on 16/12/15.
+//  Copyright © 2016年 hhly. All rights reserved.
+//
+
+#import "LYInfiniteViewCell.h"
+
+@implementation LYInfiniteViewCell
+
+@end
